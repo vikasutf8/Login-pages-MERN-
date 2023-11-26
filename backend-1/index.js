ff37446ba1d2aba57a897e846be33eb13fdac352
vikasutf8 <21bcs241@gamil.com>
2023-11-26 19:16:13 +0530

    mongoose

--- backend-1/index.js
+++ backend-1/index.js
@@ -1,8 +1,18 @@
-// require('dotenv').config()
+import dotenv from 'dotenv'
 import express from 'express'
 const app = express()
-const port =process.env.PORT ||4000;
+const port =process.env.PORT ;
+dotenv.config()
 
+
+console.log(port) 
+mongoose.connect(process.env.MONGODB_URL)
+        .then(()=>{
+          console.log('connected to MONGODB')
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
 
 
 app.listen(port, () => {
