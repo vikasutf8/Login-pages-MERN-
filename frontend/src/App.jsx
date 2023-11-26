1be4c69ee0debbb676f4230a7ea2b3b954eb0883
vikasutf8 <21bcs241@gamil.com>
2023-11-26 17:54:39 +0530

    create frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <div className='w-full h-screen bg-black text-white'>
+      vikas
+    </div>
+  )
+}
+
+export default App
