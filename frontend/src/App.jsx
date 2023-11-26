3cfc220508c8a21643f9d357d6d8c36eeee72b06
vikasutf8 <21bcs241@gamil.com>
2023-11-26 18:12:38 +0530

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Service from "./pages/Service";
 
 
 function App() {
-
-
   return (
-    <div className='w-full h-screen bg-black text-white'>
-      vikas
-    </div>
+   <BrowserRouter>
+   <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/profile" element={<Profile />}/>
+      <Route path="/sign-up" element={<SignUp />}/>
+      <Route path="/sign-in" element={<SignIn />}/>
+      <Route path="/service" element={<Service />}/>
+
+   </Routes>
+   </BrowserRouter>
   )
 }
 
