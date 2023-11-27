4863cda3ccbc7ff080fe5d8130485e24bbfcba21
vikasutf8 <21bcs241@gamil.com>
2023-11-27 11:47:56 +0530

    api routes

--- backend-1/index.js
+++ backend-1/index.js
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import  mongoose    from 'mongoose'
+import userRoutes from './routes/user.router.js'
 const app = express()
-const port =process.env.PORT ;
+const port =process.env.PORT ||4000;
 dotenv.config()
 
 
-console.log(port) 
+// console.log(port) 
 mongoose.connect(process.env.MONGODB_URL)
         .then(()=>{
           console.log('connected to MONGODB')
@@ -18,3 +20,5 @@ mongoose.connect(process.env.MONGODB_URL)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.use("/api/user",userRoutes);
